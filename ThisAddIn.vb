d47fb7491234f3cf6222cb35f52bcf3c8da7227f
LASPUMSS <umssluisalbertosolisp@gmail.com>
2022-12-02 15:53:21 -0600

    UPDATE

--- ThisAddIn.vb
+++ ThisAddIn.vb
@@ -4,7 +4,8 @@
     End Function
 
     Private Sub ThisAddIn_Startup() Handles Me.Startup
-
+        ' PERMITIR QUE EL LIBRO SE PUEDA EDITAR
+        Globals.ThisAddIn.Application.ActiveWorkbook.ChangeFileAccess(Microsoft.Office.Interop.Excel.XlFileAccess.xlReadWrite)
     End Sub
 
     Private Sub ThisAddIn_Shutdown() Handles Me.Shutdown
